d3004b2e6e69b2f0713f6b74208e3d3cbac8fc6d
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2022-08-21 22:39:32 -0300

    update

--- POO/Classe/Método estático/script.js
+++ POO/Classe/Método estático/script.js
@@ -20,10 +20,10 @@ class Produto {
     this.profundidade = profundidade;
     this.valor = valor;
   }
-  // Para declarar um método dentro de uma CLASS utilizamos o mesmo jeito que uma function normal, mas se a palavra reservada FUNCTION, pois é um método.
-  volumeProduto() {
+  // Foi declarado um método estático que não pode ser alterado. Definido tendo 3 parâmetros: largura, altura e profundidade.
+  static calculaVolume(largura, altura, profundidade) {
     // Irá retornar a multiplicação da largura, multiplicado pela altura e depois multiplicado pela profundidade.
-    return this.largura * this.altura * this.profundidade;
+    return largura * altura * profundidade;
   }
   // Método declarado chamado parcelaValor, tendo seu parâmetro chamado de parcelas. Quase o mesmo exemplo que a função anterior, a diferença é que esse tem parâmetro.
   parcelaValor(parcelas) {
@@ -37,11 +37,59 @@ let prod = new Produto('Mesa', 20, 13, 79, 100);
 // Irá apresentar no console os valores definidos no atributo da CLASS Produto.
 console.log(prod);
 
-// Para invocarmos o método que está dentro de uma classe, temos que pegar o objeto e em seguida o método junto com os "()".
-console.log(prod.volumeProduto());
+// Irá mostrar no console o resultado da multiplicação dos parâmetros presentes na CLASS Produto e dentro do método estático calculaVolume.
+// Métodos estáticos só podem ser acessados dentro da CLASS.
+console.log(Produto.calculaVolume(16, 23, 7))
 
-// Invocado o método parcelaValor e tendo o valor 2 como seu parâmetro, que no caso é o número de parcelas.
-console.log(prod.parcelaValor(2))
 
-// Não tem como acessar o método diretamente pois ele é do escopo local da CLASS, então irá retornar um erro.
-parcelaValor(4);
+
+// Foi declarado uma CLASS chamada Musica.
+class Musica {
+  // Foi criado uma lista dos atributos que vão ser usados dentro do parâmetro do construtor.
+  // Não se cria variáveis quando se trabalha com CLASS ou com atributos.
+	#titulo;
+	#duracao;
+
+  // O CONSTRUCTOR é um método especial para criar e inicializar um objeto criado a partir de uma classe.
+	constructor(titulo, duracao) {
+    // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
+		this.#titulo = titulo;
+		this.#duracao = duracao;
+	}
+
+  // Foi criado um método estático, que não pode ser alterado.
+	static converteSegundosEmMinutos(duracao) {
+		const min = Math.floor(duracao / 60)
+		let seg = duracao % 60;
+		if (seg < 10 && seg > 0) {
+			seg = `0${duracao % 60}`;
+		} else if (seg < 10) {
+				seg = '00';
+		}
+		return `${min}:${seg}`;
+	}
+
+  // Método declarado chamado getDuracao, que retorna o valor do atributo duração.
+	getDuracao() {
+    // Podemos utilizar o método de uma outra classe dentro de uma classe.
+    // Está atribuindo os valores do parâmetro do método calculaVolume presente dentro da CLASS Produto.
+    console.log(Produto.calculaVolume(2, 3, 8))
+		return this.#duracao;
+	}
+}
+
+// Foi criado uma variável chamada musica1 que está recebendo um novo valor de atributo de parâmetros da CLASS Musica.
+const musica1 = new Musica('Minha musica', 190)
+
+// Irá dar erro, pois como é estático não pode ser puxado através do seu atributo.
+// console.log(musica1.converteSegundosEmMinutos(musica1.getDuracao()));
+
+// Irá mostrar o resultado da conversão no console.
+console.log(Musica.converteSegundosEmMinutos(musica1.getDuracao()));
+
+// Podemos também alterar o valor da duração sem precisar do atributo musica1 e do método getDuracao.
+console.log(Musica.converteSegundosEmMinutos(120));
+
+
+
+
