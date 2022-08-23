5421589536e8a6aa0c905570b71df47a1b83c055
“Luiz <felipesilva9627@hotmail.com>
2022-08-23 13:25:51 -0300

    update

--- POO/Getters and Setters/script.js
+++ POO/Getters and Setters/script.js
@@ -1,47 +1,69 @@
 // Ao utilizar o USE STRICT não é possível criar elementos sem usar uma variável. Foi definido um modo estrito global.
 'use strict';
 
-// Foi declarado uma CLASS chamada Produto.
-class Produto {
-  // Foi criado uma lista dos atributos que vão ser usados dentro do parâmetro do construtor.
-  // Não se cria variáveis quando se trabalha com CLASS ou com atributos.
-  descricao;
-  largura
-  altura;
-  profundidade;
-  valor;
+// Foi declarado uma CLASS chamada Laboratório.
+class Laboratorio {
+  // Lista de atributos privados que vão ser usados dentro do construtor.
+  // Os atributos privados não podem permitem trabalhar com instancias ou atributos e não permitem alterações.
+  #nome;
+  #capacidade;
 
   // O CONSTRUCTOR é um método especial para criar e inicializar um objeto criado a partir de uma classe.
-  constructor(descricao, largura, altura, profundidade, valor) {
+  constructor(nome, capacidade) {
     // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
-    this.descricao = descricao;
-    this.largura = largura;
-    this.altura = altura;
-    this.profundidade = profundidade;
-    this.valor = valor;
+    this.#nome = nome;
+    this.#capacidade = capacidade;
   }
-  // Para declarar um método dentro de uma CLASS utilizamos o mesmo jeito que uma function normal, mas se a palavra reservada FUNCTION, pois é um método.
-  volumeProduto() {
-    // Irá retornar a multiplicação da largura, multiplicado pela altura e depois multiplicado pela profundidade.
-    return this.largura * this.altura * this.profundidade;
+
+  // O GET é utilizado no inicio do nome de um método para obter um dado de um atributo privado.
+  getNome(nome) {
+    // Irá retornar o atributo privado nome.
+    return this.#nome;
   }
-  // Método declarado chamado parcelaValor, tendo seu parâmetro chamado de parcelas. Quase o mesmo exemplo que a função anterior, a diferença é que esse tem parâmetro.
-  parcelaValor(parcelas) {
-    return this.valor / parcelas;
+  getCapacidade() {
+    // Irá retornar o atributo privado capacidade.
+    return this.#capacidade;
+  }
+
+  // Utilizado no início do nome de um método para alterar ou atribuir um dado de um atributo privado.
+  setNome(nome) {
+    // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
+    this.#nome = nome;
+  }
+  setCapacidade(capacidade) {
+    // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
+    this.#capacidade = capacidade;
   }
 }
 
-// Foi criado uma variável chamada prod que está recebendo um novo valor de atributo de parâmetros da CLASS Produto.
-let prod = new Produto('Mesa', 20, 13, 79, 100);
+// Está atribuindo valores aos atributos da CLASS Laboratorio.
+let lab = new Laboratorio('Informática', 45);
 
-// Irá apresentar no console os valores definidos no atributo da CLASS Produto.
-console.log(prod);
+// Irá apresentar UNDEFINED no console pois o atributo não pode ser acessado dessa forma.
+console.log(lab.nome);
 
-// Para invocarmos o método que está dentro de uma classe, temos que pegar o objeto e em seguida o método junto com os "()".
-console.log(prod.volumeProduto());
+// Está mostrando no console todas informações e atributos presentes na variável lab. 
+// Podemos perceber que ele acrescentou o atributo X criado fora da CLASS.
+console.log(lab)
 
-// Invocado o método parcelaValor e tendo o valor 2 como seu parâmetro, que no caso é o número de parcelas.
-console.log(prod.parcelaValor(2))
+// Está criando um valor de um atributo diretamente fora da CLASS.
+lab.x = 'Computação';
 
-// Não tem como acessar o método diretamente pois ele é do escopo local da CLASS, então irá retornar um erro.
-parcelaValor(4);
+// Irá mostrar o atributo nome no console.
+console.log(lab.x);
+
+// Está atribuindo valores aos atributos da CLASS Laboratorio.
+let lab2 = new Laboratorio('Ciência', 78);
+
+// Podemos perceber no console que o atributo X não aparece mais, pois já foi usado e atribuído  ao lab.
+console.log(lab2)
+
+// Para conseguir alterar um atributo privado temos que utilizar o set.
+lab.setNome('Novo laboratório');
+lab.setCapacidade(787);
+
+// Para obter uma informação de um atributo privado sem alterar ela, usamos o get.
+console.log(lab.getCapacidade());
+
+// Irá mostrar todos os atributos do objeto lab.
+console.log(lab);
