2e0a1a2f1813b29aad215781adea7725b1e62b40
“Luiz <felipesilva9627@hotmail.com>
2022-08-23 20:00:31 -0300

    update

--- POO/Polimorfismo/script.js
+++ POO/Polimorfismo/script.js
@@ -1,47 +1,101 @@
 // Ao utilizar o USE STRICT não é possível criar elementos sem usar uma variável. Foi definido um modo estrito global.
 'use strict';
 
-// Foi declarado uma CLASS chamada Produto.
 class Produto {
-  // Foi criado uma lista dos atributos que vão ser usados dentro do parâmetro do construtor.
-  // Não se cria variáveis quando se trabalha com CLASS ou com atributos.
-  descricao;
-  largura
-  altura;
-  profundidade;
-  valor;
+  // Lista de atributos privados que vão ser usados dentro do construtor.
+  // Os atributos privados não podem permitem trabalhar com instancias ou atributos e não permitem alterações.
+  #tipo;
+  #valor
 
   // O CONSTRUCTOR é um método especial para criar e inicializar um objeto criado a partir de uma classe.
-  constructor(descricao, largura, altura, profundidade, valor) {
+  constructor(tipo, valor) {
     // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
-    this.descricao = descricao;
-    this.largura = largura;
-    this.altura = altura;
-    this.profundidade = profundidade;
-    this.valor = valor;
+    this.#validaValor(valor);
+    this.#tipo = tipo;
+    this.#valor = valor;
   }
-  // Para declarar um método dentro de uma CLASS utilizamos o mesmo jeito que uma function normal, mas se a palavra reservada FUNCTION, pois é um método.
-  volumeProduto() {
-    // Irá retornar a multiplicação da largura, multiplicado pela altura e depois multiplicado pela profundidade.
-    return this.largura * this.altura * this.profundidade;
+  
+  // Declarado um método privado.
+  // O método privado não pode ser acessado fora da CLASS.
+  #validaValor(valor) {
+    // Se o tipo do atributo valor for diferente do tipo number ele vai retornar valor inválido.
+    if(typeof(valor) !== 'number') {
+      console.log('Valor inválido!');
+    } else {
+      console.log('Valor correto!');
+    }
   }
-  // Método declarado chamado parcelaValor, tendo seu parâmetro chamado de parcelas. Quase o mesmo exemplo que a função anterior, a diferença é que esse tem parâmetro.
-  parcelaValor(parcelas) {
-    return this.valor / parcelas;
+
+  // Declarado um método publico.
+  // O método publico pode ser acessado fora da CLASS.
+  getDados() {
+    // Irá retornar o tipo e o valor.
+    return `Tipo: ${this.#tipo}, R$ ${this.#valor}`;
+  }
+  // Utilizado no início do nome de um método para alterar ou atribuir um dado de um atributo privado.
+  setValor(valor) {
+    // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
+    this.#valor = valor;
   }
 }
 
-// Foi criado uma variável chamada prod que está recebendo um novo valor de atributo de parâmetros da CLASS Produto.
-let prod = new Produto('Mesa', 20, 13, 79, 100);
+// Está atribuindo valores nos atributos privados da CLASS Produto do objeto X.
+// Como instanciamos a CLASS Livro, ele vai armazenar tudo na própria CLASS.
+let x = new Produto('Genérico', 33);
 
-// Irá apresentar no console os valores definidos no atributo da CLASS Produto.
-console.log(prod);
+// Irá apresentar os atributos do projeto X no console.
+console.log(x);
 
-// Para invocarmos o método que está dentro de uma classe, temos que pegar o objeto e em seguida o método junto com os "()".
-console.log(prod.volumeProduto());
+// Para apresentar somente os valores dos atributos podemos invocar uma declaração já criada.
+console.log(x.getDados());
 
-// Invocado o método parcelaValor e tendo o valor 2 como seu parâmetro, que no caso é o número de parcelas.
-console.log(prod.parcelaValor(2))
+// Está setando um novo valor ao valor do atributo da CLASS Produto.
+x.setValor(123);
 
-// Não tem como acessar o método diretamente pois ele é do escopo local da CLASS, então irá retornar um erro.
-parcelaValor(4);
+// Irá apresentar os atributos do projeto X no console.
+console.log(x)
+
+// Foi declarado uma CLASS chamada Produto que tem a CLASS Livro como sua subclasse/filho.
+class Livro extends Produto {
+  // Lista de atributos privados que vão ser usados dentro do construtor.
+  // Os atributos privados não podem permitem trabalhar com instancias ou atributos e não permitem alterações.
+  #titulo;
+  #numPag;
+
+  // O CONSTRUCTOR é um método especial para criar e inicializar um objeto criado a partir de uma classe.
+  constructor(titulo, numPag, valor) {
+    // O SUPER é usado para definir os atributos ao objeto da CLASS pai, no caso Produto. Caso deixe vazio irá ficar como UNDEFINED ao reproduzir no console.
+    super('Livro', valor);
+
+    // O THIS está sendo usado para diferenciar um atributo de um parâmetro. E assim declarando o atributo da CLASS. O que vem depois do THIS é um atributo.
+    this.#titulo = titulo;
+    this.#numPag = numPag;
+  }
+
+  // Declarado um método publico.
+  // O método publico pode ser acessado fora da CLASS.
+  getDados() {
+    // Irá retornar o tipo e o valor.
+    // Está puxando o método getDados de outra CLASS, por isso tem que usar o SUPER.
+    return `
+    ${super.getDados()}
+    Titulo: ${this.#titulo}
+    Numero de páginas: ${this.#numPag}`
+  }
+}
+
+// Está atribuindo valores nos atributos privados da CLASS Livro do objeto X.
+// Como instanciamos a CLASS Livro, ele vai armazenar tudo na própria CLASS.
+let y = new Livro('Meu livro', 122, 33);
+
+// Irá apresentar os atributos do projeto X no console.
+console.log(y);
+
+// Para apresentar somente os valores dos atributos podemos invocar uma declaração já criada.
+console.log(y.getDados());
+
+// Está setando um novo valor ao valor do atributo da CLASS Produto.
+x.setValor(123);
+
+// Irá apresentar os atributos do projeto X no console.
+console.log(y)
